f33796576a43174977c39329d861b2f09fdf393c
Vaibhav Joshi <lynxofficial88@gmail.com>
2026-04-30 06:11:21 +0530

    day-4 done

--- Day-04/src/app.js
+++ Day-04/src/app.js
@@ -3,3 +3,26 @@ const express = require("express")
 const app = express()
 
 module.exports = app
+
+const notes =[]
+
+app.use(express.json())
+
+app.get("/notes",(req,res)=>{
+  res.send(notes)
+})
+
+app.post("/notes",(req,res)=>{
+  notes.push(req.body)
+  res.send("Added Successfully")
+})
+
+app.delete("/notes/:index",(req,res)=>{
+  delete notes[req.params.index]
+  res.send("Deleted Successfully")
+})
+
+app.patch("/notes/:index",(req,res)=>{
+  notes[req.params.index].descryption = req.body.descryption
+  res.send("Updated")
+})
